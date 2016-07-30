61998fe71a0fe8111949606469877d8db4318358
Christopher Millward <christopher.millward@gmail.com>
2016-07-30 14:43:12 -0400

    Added partial saferoom implementation.

---
+++ Assets/Scripts/SafeRoomEntrance.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SafeRoomEntrance : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player") {
+            OnSafeRoomEnter ();
+        }
+    }
+
+    void OnSafeRoomEnter() {
+        Debug.Log ("Entered the safe room.");
+    }
+}
---
+++ Assets/Scripts/SafeRoomExit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SafeRoomExit : MonoBehaviour {
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.tag == "Player") {
+            OnSafeRoomExit ();
+        }
+    }
+
+    void OnSafeRoomExit() {
+        Debug.Log ("Exited the safe room.");
+    }
+}
