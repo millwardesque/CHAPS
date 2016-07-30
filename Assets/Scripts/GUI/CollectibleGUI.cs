88fbe0a0a6ca84d7d225254ff658da44b8620364
Christopher Millward <christopher.millward@gmail.com>
2016-07-30 08:42:15 -0400

    Added collectible intel.

---
+++ Assets/Scripts/GUI/CollectibleGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CollectibleGUI : MonoBehaviour {
+    public Text valueLabel;
+
+    void Start() {
+        GameManager.Instance.Messenger.AddListener ("IntelPointsChanged", OnValueChanged);
+        UpdateValue (0);
+    }
+
+    void OnValueChanged(Message message) {
+        int newValue = (int)message.data;
+        UpdateValue (newValue);
+    }
+
+    void UpdateValue(int newValue) {
+        valueLabel.text = newValue.ToString ();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ using Rewired;
 public class PlayerController : InputController {
     Player m_player;    // Rewired player.
 
+    int m_intelPointsCollected = 0;
+    public int IntelPointsCollected {
+        get { return m_intelPointsCollected; }
+        set {
+            m_intelPointsCollected = value;
+            GameManager.Instance.Messenger.SendMessage (this, "IntelPointsChanged", m_intelPointsCollected);
+        }
+    }
+
     void Awake() {
         m_player = ReInput.players.GetPlayer(0);
     }
@@ -22,5 +31,12 @@ public class PlayerController : InputController {
 
     public override float GetAxis (string axisName) {
         return m_player.GetAxis (axisName);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.collider.GetComponent<IntelCollectible>()) {
+            IntelPointsCollected += col.collider.GetComponent<IntelCollectible> ().intelPoints;
+            Destroy (col.collider.gameObject);   
+        }
     }
 }
