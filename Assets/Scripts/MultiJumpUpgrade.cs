6d5f68b379850e1fcf3c56ad718555a014922373
Christopher Millward <cmillward@millwardesque.com>
2016-08-01 14:54:35 -0400

    Added prototype upgrade for double-jump.

---
+++ Assets/Scripts/MultiJumpUpgrade.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiJumpUpgrade : Powerup {
+    public int numTotalJumps = 2;
+
+    void Start() {
+        duration = -1f;
+    }
+
+    protected override void OnTriggered() {
+        base.OnTriggered();
+
+        GameManager.Instance.Player.maxJumps = numTotalJumps;
+    }
+}
