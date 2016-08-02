ea5e0147e7b7452fc458312ab1e91a1deb6cc881
Christopher Millward <christopher.millward@gmail.com>
2016-08-02 07:02:09 -0400

    Made Restart button on game-over gain focus and respond to space / gamepad button presses.

--- Assets/Scripts/GUI/GameOverGUI.cs
+++ Assets/Scripts/GUI/GameOverGUI.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class GameOverGUI : MonoBehaviour {
+    void Start() {
+        Button restartButton = GetComponentInChildren<Button> ();
+        EventSystem.current.SetSelectedGameObject (restartButton.gameObject);        
+    }
+
     public void OnRestartGameClick() {
         GameManager.Instance.RestartGame();
         Destroy(gameObject);
