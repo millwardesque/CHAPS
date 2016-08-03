5b1cffe3eb5d38b8d852f15a660887e607c06406
Christopher Millward <christopher.millward@gmail.com>
2016-08-03 13:32:14 -0400

    Added door-close sound to saferoom.

--- Assets/Scripts/SafeRoomEntrance.cs
+++ Assets/Scripts/SafeRoomEntrance.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class SafeRoomEntrance : MonoBehaviour {
+    public AudioClip doorCloseSound;
+
     void OnTriggerExit2D(Collider2D col) {
         if (col.tag == "Player") {
             OnSafeRoomEnter ();
@@ -11,6 +13,7 @@ public class SafeRoomEntrance : MonoBehaviour {
     void OnSafeRoomEnter() {
         GetComponent<Collider2D> ().isTrigger = false;
         GetComponent<Collider2D> ().usedByEffector = true;
+        GameManager.Instance.Player.GetComponent<AudioSource> ().PlayOneShot (doorCloseSound);
         Debug.Log ("Entered the safe room.");
     }
 }
--- Assets/Scripts/SafeRoomExit.cs
+++ Assets/Scripts/SafeRoomExit.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class SafeRoomExit : MonoBehaviour {
+    public AudioClip doorCloseSound;
+
     void OnTriggerExit2D(Collider2D col) {
         if (col.tag == "Player" && col.bounds.min.x >= GetComponent<Collider2D>().bounds.max.x) {
             OnSafeRoomExit ();
@@ -12,5 +14,6 @@ public class SafeRoomExit : MonoBehaviour {
         Debug.Log ("Exited the safe room.");
         GetComponent<Collider2D> ().isTrigger = false;
         GetComponent<Collider2D> ().usedByEffector = true;
+        GameManager.Instance.Player.GetComponent<AudioSource> ().PlayOneShot (doorCloseSound);
     }
 }
