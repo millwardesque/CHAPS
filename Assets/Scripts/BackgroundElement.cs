2b8979b4bc1b7f77dd0b81ff286107c8e8de2afe
Christopher Millward <christopher.millward@gmail.com>
2016-08-15 12:51:00 -0400

    Added background gradient and sample building.

---
+++ Assets/Scripts/BackgroundElement.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu (menuName = "Background/Background Element")]
+public class BackgroundElement : ScriptableObject {
+    public Sprite sprite;
+}
